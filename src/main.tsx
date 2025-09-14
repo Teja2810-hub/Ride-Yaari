e2eb0574a93484351abd44735000e9ec9f46b720
Teja2810-hub <76026521+Teja2810-hub@users.noreply.github.com>
2025-09-14 13:55:15 -0400

    disable right click

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+//right click disable
+// window.addEventListener('contextmenu', (e) => {
+//   e.preventDefault()
+// })
+//end code right click 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
